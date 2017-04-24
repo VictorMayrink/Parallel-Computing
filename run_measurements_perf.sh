30f661d5b888fd108e4d030b80bd2578d6af83e5
Victor Mayrink <mayrink.vtm@gmail.com>
2017-04-24 09:17:52 -0300

    changes on run_measurements_perf.sh

--- run_measurements_perf.sh
+++ run_measurements_perf.sh
@@ -16,32 +16,29 @@ mkdir results_perf/mandelbrot_seq
 SIZE=$INITIAL_SIZE
 for ((i=1; i<=$ITERATIONS; i++)); do
     for ((r = 1; r<=MEASUREMENTS; r++)); do
-        perf stat -d ./mandelbrot_seq_withIO -2.500  1.500 -2.000 2.000 $SIZE >> full.log 2>&1
-        perf stat -d ./mandelbrot_seq_withIO -0.800 -0.700  0.050 0.150 $SIZE >> seahorse.log 2>&1
-        perf stat -d ./mandelbrot_seq_withIO  0.175  0.375 -0.100 0.100 $SIZE >> elephant.log 2>&1
-        perf stat -d ./mandelbrot_seq_withIO -0.188 -0.012  0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+        perf stat ./mandelbrot_seq_withIO -2.500  1.500 -2.000 2.000 $SIZE >> full.log 2>&1
+        perf stat ./mandelbrot_seq_withIO -0.800 -0.700  0.050 0.150 $SIZE >> seahorse.log 2>&1
+        perf stat ./mandelbrot_seq_withIO  0.175  0.375 -0.100 0.100 $SIZE >> elephant.log 2>&1
+        perf stat ./mandelbrot_seq_withIO -0.188 -0.012  0.554 0.754 $SIZE >> triple_spiral.log 2>&1
     done
     SIZE=$(($SIZE * 2))
 done
 
-mv *.log results_perf/mandelbrot_seq
 rm output.ppm
 
 #MANDELBROT: Sequential (without I/O)
-mkdir results_perf/mandelbrot_seq
 SIZE=$INITIAL_SIZE
 for ((i=1; i<=$ITERATIONS; i++)); do
     for ((r = 1; r<=MEASUREMENTS; r++)); do
-        perf stat -d ./mandelbrot_seq_withoutIO -2.500  1.500 -2.000 2.000 $SIZE >> full.log 2>&1
-        perf stat -d ./mandelbrot_seq_withoutIO -0.800 -0.700  0.050 0.150 $SIZE >> seahorse.log 2>&1
-        perf stat -d ./mandelbrot_seq_withoutIO  0.175  0.375 -0.100 0.100 $SIZE >> elephant.log 2>&1
-        perf stat -d ./mandelbrot_seq_withoutIO -0.188 -0.012  0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+        perf stat ./mandelbrot_seq_withoutIO -2.500  1.500 -2.000 2.000 $SIZE >> full.log 2>&1
+        perf stat ./mandelbrot_seq_withoutIO -0.800 -0.700  0.050 0.150 $SIZE >> seahorse.log 2>&1
+        perf stat ./mandelbrot_seq_withoutIO  0.175  0.375 -0.100 0.100 $SIZE >> elephant.log 2>&1
+        perf stat ./mandelbrot_seq_withoutIO -0.188 -0.012  0.554 0.754 $SIZE >> triple_spiral.log 2>&1
     done
     SIZE=$(($SIZE * 2))
 done
 
 mv *.log results_perf/mandelbrot_seq
-rm output.ppm
 
 #MANDELBROT: OpenMP (without I/O)
 mkdir results_perf/mandelbrot_omp
@@ -50,10 +47,10 @@ for THREADS in ${NTHREADS[@]}; do
 
     for ((i=1; i<=$ITERATIONS; i++)); do
         for ((r = 1; r<=MEASUREMENTS; r++)); do
-            perf stat -d ./mandelbrot_omp_withoutIO -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
-            perf stat -d ./mandelbrot_omp_withoutIO -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
-            perf stat -d ./mandelbrot_omp_withoutIO  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
-            perf stat -d ./mandelbrot_omp_withoutIO -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triple_spiral.log 2>&1
+            perf stat ./mandelbrot_omp_withoutIO -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
+            perf stat ./mandelbrot_omp_withoutIO -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
+            perf stat ./mandelbrot_omp_withoutIO  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
+            perf stat ./mandelbrot_omp_withoutIO -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triple_spiral.log 2>&1
         done
         SIZE=$(($SIZE * 2))
     done
@@ -63,7 +60,6 @@ for THREADS in ${NTHREADS[@]}; do
 done
 
 mv *.log results_perf/mandelbrot_omp
-rm output.ppm
 
 #MANDELBROT: Pthreads (without I/O)
 mkdir results_perf/mandelbrot_pth
@@ -72,10 +68,10 @@ for THREADS in ${NTHREADS[@]}; do
 
     for ((i=1; i<=$ITERATIONS; i++)); do
         for ((r = 1; r<=MEASUREMENTS; r++)); do
-            perf stat -d ./mandelbrot_pth_withoutIO -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
-            perf stat -d ./mandelbrot_pth_withoutIO -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
-            perf stat -d ./mandelbrot_pth_withoutIO  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
-            perf stat -d ./mandelbrot_pth_withoutIO -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triple_spiral.log 2>&1
+            perf stat ./mandelbrot_pth_withoutIO -2.500  1.500 -2.000 2.000 $THREADS $SIZE >> full.log 2>&1
+            perf stat ./mandelbrot_pth_withoutIO -0.800 -0.700  0.050 0.150 $THREADS $SIZE >> seahorse.log 2>&1
+            perf stat ./mandelbrot_pth_withoutIO  0.175  0.375 -0.100 0.100 $THREADS $SIZE >> elephant.log 2>&1
+            perf stat ./mandelbrot_pth_withoutIO -0.188 -0.012  0.554 0.754 $THREADS $SIZE >> triple_spiral.log 2>&1
         done
         SIZE=$(($SIZE * 2))
     done
@@ -85,4 +81,3 @@ for THREADS in ${NTHREADS[@]}; do
 done
 
 mv *.log results_perf/mandelbrot_pth
-rm output.ppm
